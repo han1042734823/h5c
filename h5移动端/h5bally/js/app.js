69261480e2b2d1a5ffcead07a53c8db962b25b68
纳雅 <41954639+han1042734823@users.noreply.github.com>
2019-10-31 12:14:27 +0800

    Add files via upload

--- h5移动端/h5bally/js/app.js
+++ h5移动端/h5bally/js/app.js
@@ -222,34 +222,6 @@
     });
 
 
-
-
-    // 缩放
-    try {
-    // 禁用双击缩放
-    document.addEventListener("touchstart", function(event) {
-      if (event.touches.length > 1) {
-        event.preventDefault();
-      }
-    });
-    var lastTouchEnd = 0;
-    document.addEventListener(
-      "touchend",
-      function(event) {
-        var now = new Date().getTime();
-        if (now - lastTouchEnd <= 300) {
-          event.preventDefault();
-        }
-        lastTouchEnd = now;
-      },
-      false
-    );
-    // 禁用双指手势操作
-    document.addEventListener("gesturestart", function(event) {
-      event.preventDefault();
-    });
-  } catch (error) {}
-
   //移动端适配
   (function change(){
     var oFz = document.getElementsByTagName("html")[0];
