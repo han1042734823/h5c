9d4b7aff391544b9d618ba9143f517fda7881e2f
纳雅 <41954639+han1042734823@users.noreply.github.com>
2019-10-31 16:00:42 +0800

    Add files via upload

--- h5移动端/h5bally/js/app.js
+++ h5移动端/h5bally/js/app.js
@@ -248,4 +248,26 @@
     var width = window.innerWidth;
     oFz.style.fontSize = width/10 +"px";
     window.onresize=function(){change();};
-    })();
+  })();
+  //阻止Safari等浏览器 页面缩放
+  window.onload = function() {
+    var lastTouchEnd = 0;
+    document.addEventListener('touchstart', function(event) {
+        if (event.touches.length > 1) {
+            event.preventDefault();
+        }
+    });
+    document.addEventListener('touchend', function(event) {
+        var now = (new Date()).getTime();
+        if (now - lastTouchEnd <= 300) {
+            event.preventDefault();
+        }
+        lastTouchEnd = now;
+    }, false);
+    document.addEventListener('gesturestart', function(event) {
+        event.preventDefault();
+    });
+    document.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+	})
+}
