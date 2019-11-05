667df5bd8e496d300e26b92abfd7f7e4c7f7f049
纳雅 <41954639+han1042734823@users.noreply.github.com>
2019-11-05 11:12:29 +0800

    Add files via upload

--- h5移动端/h5bally/js/app.js
+++ h5移动端/h5bally/js/app.js
@@ -494,25 +494,25 @@
     oFz.style.fontSize = width/10 +"px";
     window.onresize=function(){change();};
 })();
-//阻止Safari等浏览器 页面缩放
-window.onload = function() {
-    // 阻止双击放大
-    var lastTouchEnd = 0;
-    document.addEventListener('touchstart', function(event) {
-        if (event.touches.length > 1) {
-            event.preventDefault();
-        }
-    });
-    document.addEventListener('touchend', function(event) {
-        var now = (new Date()).getTime();
-        if (now - lastTouchEnd <= 300) {
-            event.preventDefault();
-        }
-        lastTouchEnd = now;
-    }, false);
-
-    // 阻止双指放大
-    document.addEventListener('gesturestart', function(event) {
-        event.preventDefault();
-    });
-}
+// //阻止Safari等浏览器 页面缩放
+// window.onload = function() {
+//     // 阻止双击放大
+//     var lastTouchEnd = 0;
+//     document.addEventListener('touchstart', function(event) {
+//         if (event.touches.length > 1) {
+//             event.preventDefault();
+//         }
+//     });
+//     document.addEventListener('touchend', function(event) {
+//         var now = (new Date()).getTime();
+//         if (now - lastTouchEnd <= 300) {
+//             event.preventDefault();
+//         }
+//         lastTouchEnd = now;
+//     }, false);
+//
+//     // 阻止双指放大
+//     document.addEventListener('gesturestart', function(event) {
+//         event.preventDefault();
+//     });
+// }
